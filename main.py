2cc51c754fe3072661dd4db76f574f3ce5b4c65b
reza <darzi.reza@gmail.com>
2025-10-06 15:22:50 +0330

    refactor: Use uv instead pip

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import mplfinance as mpf
 
 import yfinance as yf
 tickers = ["AAPL", "MSFT", "GOOG"]
-data = yf.download(tickers, start="2020-01-01")["Adj Close"]
+# data = yf.download(tickers, start="2020-01-01")["Adj Close"]
+data = yf.download(tickers, start="2025-10-04")
+
 returns = data.pct_change().dropna()
 
+print(returns.head())
